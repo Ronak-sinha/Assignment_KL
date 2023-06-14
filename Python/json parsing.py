486daa76a6ae80d793f9999616d8bad3f24a0d35
Ronak Sinha <ronaksinha1998@gmail.com>
2023-06-14 16:28:00 +0530

    Update json parsing.py

--- Python/json parsing.py
+++ Python/json parsing.py
@@ -1,12 +1,15 @@
 import json
+import sys
 
 
 def json_parsing():
-    """from file we are parsing the data min, max and avg value of every 
+    """
+    from file, we are parsing the data min, max and avg value of every
     parameter and store in output_data_res.json
     """
-    try: 
-        with open("sample_data.json", "r") as data_file:
+    path = "sample_data.json"
+    try:
+        with open(path, "r") as data_file:
             data = json.load(data_file)
 
         output_data = []
@@ -14,17 +17,17 @@ def json_parsing():
         para_list = data["parametersList"]
 
         for para_name in para_list:
-            value1 = {'parameterName': para_name["parameterName"],
-                    'min_value': para_name["min"], 'man_value': para_name["max"],
-                    'avg_value': para_name["avg"]}
+            value1 = {"parameterName": para_name["parameterName"],
+                      "min_value": para_name["min"], "man_value": para_name["max"],
+                      "avg_value": para_name["avg"]}
             output_data.append(value1)
         with open("output_data_res.json", "w") as output_data_file:
             json.dump(output_data, output_data_file, indent=2)
 
         data_file.close()
         output_data_file.close()
-    except Exception as e:
-        print(e)
+    except FileNotFoundError:
+        print(f"File {path} not found", file=sys.stderr)
 
 
 if __name__ == "__main__":
